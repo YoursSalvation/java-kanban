c2d39eb663010ead989e4397de2955b93698f468
Dmitry Prokhorov <prokhorovds26@mail.ru>
2025-02-14 21:54:36 +0500

    add test for control epic duration, fix static test in EpicTest

--- test/task/EpicTest.java
+++ test/task/EpicTest.java
@@ -1,6 +1,7 @@
 package task;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -15,14 +16,19 @@ class EpicTest {
     static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm");
 
     @BeforeAll
-    static void CreateSubTasks() {
+    static void create_sub_tasks() {
         SubTask subTask1 = new SubTask(new Task("SubTask1", "description1", 2, Status.NEW
                 , Duration.ofMinutes(120), LocalDateTime.parse("12.12.2025, 12:00", formatter)), 1);
         SubTask subTask2 = new SubTask(new Task("SubTask2", "description2", 3, Status.NEW
-                , Duration.ofMinutes(12), LocalDateTime.parse("21.12.2025, 10:45")), 1);
+                , Duration.ofMinutes(12), LocalDateTime.parse("21.12.2025, 10:45", formatter)), 1);
         subTasks.put(subTask1.getId(), subTask1);
         subTasks.put(subTask2.getId(), subTask2);
         epic.setSubTasks(subTasks);
         assertEquals(subTasks, epic.getSubTasks());
+    }
+
+    @Test
+    void check_duration_correct_work() {
+        assertEquals(132, epic.duration.toMinutes());
     }
 }
